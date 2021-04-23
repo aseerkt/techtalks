62ccbd5b26fb0c353c1a0e3531af9968880c4885
Aseer KT <aseermiqdad@gmail.com>
2021-04-23 12:58:15 +0530

    Added strapi backend

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ef9a62c250cc8221c00ea1427ca5ac3c'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9dab73c6-5560-4bb0-89ce-58d3bf0104b1'
+};
